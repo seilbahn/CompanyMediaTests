75d617f67f852589f6bcde3c7a1c489768c8ab06
seilbahn <tomassinioi@gmail.com>
2024-07-18 12:14:11 +0300

    Renaming Beaver -> Notetaker. Documenting the class Notetaker

--- CompanyMediaTests/Notetaker.cs
+++ CompanyMediaTests/Notetaker.cs
@@ -3,21 +3,42 @@ using Serilog.Core;
 
 namespace CompanyMediaTests
 {
+    /// <summary>
+    /// The class Notetaker is designed to
+    /// implement logging via the library Serilog.
+    /// </summary>
     internal class Notetaker
     {
-        public LoggerConfiguration LoggerConfiguration { get; set; }
+        /// <summary>
+        /// The reference to the class LoggerConfiguration.
+        /// </summary>
+        public LoggerConfiguration Configuration { get; set; }
 
+
+        /// <summary>
+        /// The reference to the class Logger.
+        /// </summary>
         public Logger Logger { get; set; }
 
-        public string Path { get; }
+        /// <summary>
+        /// The path to the log file.
+        /// </summary>
+        public string Path { get; set; }
 
+
+        /// <summary>
+        /// The constructor initializes a new instance of the class Notetaker.<br/>
+        /// </summary>
+        /// <param name="logFilePath">
+        /// The path to the log file.
+        /// </param>
         public Notetaker(string logFilePath)
         {
             Path = logFilePath;
-            LoggerConfiguration = new LoggerConfiguration();
-            LoggerConfiguration = LoggerConfiguration.WriteTo.Console().WriteTo.File(logFilePath);
-            LoggerConfiguration = LoggerConfiguration.MinimumLevel.Information();
-            Logger = LoggerConfiguration.CreateLogger();
+            Configuration = new LoggerConfiguration();
+            Configuration = Configuration.WriteTo.Console().WriteTo.File(logFilePath);
+            Configuration = Configuration.MinimumLevel.Information();
+            Logger = Configuration.CreateLogger();
         }
     }
 }
